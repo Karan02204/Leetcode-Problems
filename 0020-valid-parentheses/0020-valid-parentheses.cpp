a369761c6488a34d95cc2b5fed2cd72e5191b8cc
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2025-07-23 10:05:20 +0530

    Time: 0 ms (100%), Space: 8.6 MB (83.42%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c : s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            } else{
+                
+                if(st.empty()){
+                    return false;
+                }
+
+                char temp = st.top();
+                st.pop();
+
+
+                if((c==')' && temp!='(') || (c=='}' && temp!='{') || (c == ']' && temp!='[') ){
+                    return false;
+                }
+            }
+        }
+        
+        return st.empty();
+    }
+};
