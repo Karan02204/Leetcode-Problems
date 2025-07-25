d542818eb7abc86a36a9c7a76709df80156c7720
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2025-07-25 23:06:50 +0530

    Time: 0 ms (100%), Space: 9.4 MB (98.83%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -5,10 +5,10 @@ public:
 
         for(int i=0;i<numRows;i++){
             vector<int> temp(i+1,1);
+
             for(int j=1;j<i;j++){
                 temp[j] = result[i-1][j-1] + result[i-1][j];
             }
-
             result.push_back(temp);
         }
 
