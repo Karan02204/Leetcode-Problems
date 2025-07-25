105a94eade7e6b6e2c37ff566317154761c78956
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2025-07-25 23:40:31 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.15%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -13,14 +13,14 @@ public:
 
             if(nums[low] <= nums[mid]){
                 if(nums[low] <= target && target <= nums[mid]){
-                    high = mid-1;
+                    high = mid -1;
                 } else{
                     low = mid+1;
                 }
             } else{
-                if(nums[mid] < target && target<=nums[high]){
+                if(nums[mid] < target && target <=nums[high]){
                     low = mid+1;
-                } else {
+                } else{
                     high = mid-1;
                 }
             }
