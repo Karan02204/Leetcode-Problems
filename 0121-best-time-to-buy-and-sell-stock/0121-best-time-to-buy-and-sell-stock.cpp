1d95c1aecf5ee89596a98a67edebd0a4c896c0cc
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2025-07-25 22:43:42 +0530

    Time: 0 ms (100%), Space: 97.3 MB (86.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,7 +5,7 @@ public:
         int min = INT_MAX;
 
         for(int i=0;i<prices.size();i++){
-            if(prices[i] < min){
+            if(prices[i] < min ){
                 min = prices[i];
             } else if(prices[i] - min > profit){
                 profit = prices[i] - min;
