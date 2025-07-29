9cc1ead5a5a5e6360fb31f23609f599093154214
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2025-07-29 09:32:36 +0530

    Time: 3 ms (90.25%), Space: 13 MB (62.02%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-       //if both the values are same than take diagonally previous element dp[i-1][j-1]. if different then take min(up , left ,and diagonally previous element);
+        //if both the values are same than take diagonally previous element dp[i-1][j-1]. if different then take min(up , left ,and diagonally previous element);
 
-       int n=word1.size();
-       int m = word2.size();
+        int n = word1.size();
+        int m = word2.size();
 
-       vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
 
-       for(int i=0;i<=n;i++){
-            dp[i][0] = i;
-       }
-
-       for(int i=0;i<=m;i++){
-            dp[0][i] = i;
-       }
-
-       for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(word1[i-1] == word2[j-1]){
-                dp[i][j] = dp[i-1][j-1];
-            } else {
-                int temp = min(dp[i-1][j],dp[i][j-1]);
-                dp[i][j] = min(temp,dp[i-1][j-1])+1;
-            }
-        }
-       }
         for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                cout << dp[i][j] << " ";
-            }
-            cout << endl;
+            dp[i][0] = i;
         }
 
-       return dp[n][m]; 
+        for(int j=0;j<=m;j++){
+            dp[0][j] = j;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                } else{
+                    int temp = min(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = min(temp,dp[i-1][j-1]) + 1;
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
