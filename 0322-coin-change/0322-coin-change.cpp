97d2452a83342cc23e1a41e3d79aa93f142604c2
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2025-07-29 07:22:37 +0530

    Time: 44 ms (42.61%), Space: 53.1 MB (13.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,22 +4,23 @@ public:
         int n = coins.size();
         int m = amount;
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=1;i<=m;i++){
-            dp[0][i] = INT_MAX - 1 ;
+        vector<vector<int>> dp(n+1 , vector<int>(m+1,0));
+
+        for(int j=1;j<=m;j++){
+            dp[0][j] = INT_MAX - 1;
         }
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(coins[i-1] > j){
                     dp[i][j] = dp[i-1][j];
-                } else if (coins[i-1] <= j){
-                    int remaining_balls = j-coins[i-1];
-                    dp[i][j] = min(dp[i-1][j],1+dp[i][remaining_balls]);
+                } else if(coins[i-1] <= j){
+                    int remaining_balls = j - coins[i-1];
+                    dp[i][j] = min(dp[i-1][j] , 1+dp[i][remaining_balls]);
                 }
             }
         }
 
-        return dp[n][m] == INT_MAX-1 ? -1 : dp[n][m];
+        return dp[n][m] == INT_MAX -1 ? -1 : dp[n][m];
     }
 };
