c461116550ed3dc481fd66965b1f2bb635d939b5
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2025-08-24 16:17:56 +0530

    Time: 1 ms (6.65%), Space: 8.7 MB (18.63%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<=1){
+        if(n <= 1){
             return 1;
         }
 
         vector<int> store(n+1);
         store[0] = 1;
         store[1] = 1;
+
         for(int i=2;i<=n;i++){
-            store[i] = store[i-1] + store[i-2];
+            store[i] = store[i-1] + store[i-2]; 
         }
 
         return store[n];
