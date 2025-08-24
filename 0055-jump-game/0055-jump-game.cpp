c21bcc35aeebde3ac8f0764a1cb619cbaa010c00
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2025-08-24 17:15:42 +0530

    Time: 0 ms (100%), Space: 52.4 MB (34.45%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int reached  = nums.size()-1;
-        for(int i=reached;i>=0;i--){
-            if(nums[i]+i>=reached){
+        int reached = nums.size() - 1;
+        for(int i= reached ; i >= 0 ; i--){
+            if(nums[i]+i >= reached){
                 reached = i;
             }
         }
-
-        if(reached == 0){
-            return true;
-        }
-
+        
+        if(reached == 0) return true;
         return false;
     }
 };
