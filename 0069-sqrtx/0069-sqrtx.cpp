1d4e5784e393800550c9116d361fc34f7b886e79
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2025-10-27 12:22:45 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.85%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -5,18 +5,17 @@ public:
         long int high = x;
         long int ans = 0;
         while(low<=high){
-            long int mid = (low+high)/2;
-            if((long long)mid*mid == x){
+            long mid = (low+high)/2;
+            if(mid*mid == x){
                 return mid;
             }
-            if(x>(long long)mid*mid ){
+            if(x>mid*mid ){
                 low = mid+1;
                 ans = mid;
             } else {
                 high = mid-1;
             }
         }
-
         return ans;
     }
 };
