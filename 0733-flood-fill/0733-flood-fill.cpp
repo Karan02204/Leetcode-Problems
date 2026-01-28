751b136229320f8bfc9a84b294a15bd9bc8d820d
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2026-01-29 01:27:39 +0530

    Time: 0 ms (100%), Space: 18.2 MB (55.67%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    void DFS(vector<vector<int>>& image , int i , int j , int r , int c , int oldColor , int newColor){
-        if( i == r || j == c || i<0 || j < 0 || image[i][j]!=oldColor || image[i][j] == newColor){
-            return;
-        }
-
-        image[i][j] = newColor;
-
-        DFS(image , i , j+1 , r , c , oldColor , newColor);
-        DFS(image , i-1 , j , r , c , oldColor , newColor);
-        DFS(image , i , j-1 , r , c , oldColor , newColor);
-        DFS(image , i+1 , j , r , c , oldColor , newColor);
-    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int r = image.size();
+        int r= image.size();
         int c = image[0].size();
 
-        int oldColor = image[sr][sc];
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        int pixel = image[sr][sc];
+        image[sr][sc] = color;
 
-        DFS(image , sr , sc , r , c , oldColor , color);
+        vector<int> dx = {0,-1,0,1};
+        vector<int> dy = {1,0,-1,0};
 
+        while(!q.empty()){
+            int size = q.size();
+
+            for(int i=0;i<size;i++){
+                pair<int,int> node = q.front();
+                q.pop();
+
+                for(int j=0;j<4;j++){
+                    int x = node.first + dx[j];
+                    int y = node.second + dy[j];
+
+                    if(x>=0 && y>=0 && x<r && y<c && image[x][y] == pixel  && image[x][y]!=color){
+                        q.push({x,y});
+                        image[x][y] = color;
+                    }
+                }
+            }
+        }
 
         return image;
     }
