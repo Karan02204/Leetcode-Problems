b9a023ef3792e55664f018bcb76f36e3da95e880
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2026-01-31 10:30:32 +0530

    Time: 0 ms (100%), Space: 9.9 MB (61.75%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    void solver(vector<int>& nums , vector<int>& temp , vector<vector<int>>& result , int index){
+    void func(vector<int>& nums,vector<vector<int>>& result,vector<int>& temp,int index){
         result.push_back(temp);
 
         for(int i=index;i<nums.size();i++){
             temp.push_back(nums[i]);
-            solver(nums,temp,result,i+1);
+            func(nums,result,temp,i+1);
             temp.pop_back();
-        }
 
+        }
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
+        int start = 0;
         vector<int> temp;
-        solver(nums,temp,result,0);
+        func(nums,result,temp,start);
         return result;
     }
 };
