2d1e76fc05f61eeaf94f552009e928d63f5b7103
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2026-01-31 10:10:41 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.6%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& memo) {
-        if (n <= 1) return 1;
-        if (memo[n] != -1) return memo[n];
-
-        return memo[n] = solve(n - 1, memo) + solve(n - 2, memo);
-    }
-
     int climbStairs(int n) {
-        vector<int> memo(n + 1, -1);
-        return solve(n, memo);
+        if(n<=1){
+            return 1;
+        }
+
+        vector<int> store(n+1);
+        store[0] = 1;
+        store[1] = 1;
+        for(int i=2;i<=n;i++){
+            store[i] = store[i-1] + store[i-2];
+        }
+
+        return store[n];
     }
 };
