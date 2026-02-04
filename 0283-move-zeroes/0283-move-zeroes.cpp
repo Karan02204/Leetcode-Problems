d0d54140a4082fa102ba7a6a215b10fb4d20de11
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2026-02-04 09:50:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int x=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                nums[x++] = nums[i];
-            }
-        }
+        int i=0;
+        int j=1;
 
-        while(x<nums.size()){
-            nums[x++] = 0;
+        while(j<nums.size()){
+            if(nums[i] == 0 && nums[j] == 0){
+                j++;
+            } else if(nums[i] == 0){
+                swap(nums[i],nums[j]);
+                i++;
+            }
         }
     }
 };
