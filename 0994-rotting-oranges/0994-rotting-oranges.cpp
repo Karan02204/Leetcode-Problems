f800df7ea491eb21a9a7f1ba4c0b62503d5b04a0
Karan Attri <134791021+Karan02204@users.noreply.github.com>
2026-02-18 12:41:24 +0530

    Time: 0 ms (100%), Space: 16.8 MB (72.89%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,50 +2,47 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int r = grid.size();
-        int c =grid[0].size();
-
+        int c = grid[0].size();
         int fresh = 0;
+
         queue<pair<int,int>> q;
 
         for(int i=0;i<r;i++){
-            for(int j=0 ; j<c ;j++){
-                if(grid[i][j] == 1){
-                    fresh++;
-                } else if(grid[i][j] == 2){
+            for(int j=0;j<c;j++){
+                if(grid[i][j] == 1) fresh++;
+                else if( grid[i][j] == 2){
                     q.push({i,j});
                 }
             }
         }
 
+        vector<int> dx = {0 , -1 , 0 ,1};
+        vector<int> dy = {1 , 0 , -1 ,0};
         int min = 0;
-
-        vector<int> dx = {0,-1,0,1};
-        vector<int> dy = {1,0,-1,0};
 
         while(!q.empty()){
             int size = q.size();
 
             for(int i=0;i<size;i++){
-                pair<int,int> node = q.front();
+                pair<int , int> node = q.front();
                 q.pop();
 
                 for(int j=0;j<4;j++){
                     int x = node.first + dx[j];
                     int y = node.second + dy[j];
 
-                    if(x>=0 && y>=0 && x<r && y<c && grid[x][y] == 1){
-                        q.push({x,y});
+                    if(x >=0 && y>=0 && x<r && y<c && grid[x][y] == 1){
                         grid[x][y] = 2;
+                        q.push({x,y});
                         fresh--;
                     }
                 }
             }
 
-            if(q.size()>0) min++;
+            if(q.size() > 0) min++;
         }
 
         if(fresh == 0) return min;
-
         return -1;
     }
 };
