3c4372ed0ccdb52ed7ee7f555406426798941967
Karan <134791021+Karan02204@users.noreply.github.com>
2026-03-09 15:40:16 +0530

    Time: 0 ms (100%), Space: 34.4 MB (11.1%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -22,7 +22,7 @@ public:
         if(!root) return NULL;
 
         if(key > root->val){
-            root->right = deleteNode(root->right,key);
+            root->right = deleteNode(root->right , key);
         } else if(key < root->val){
             root->left = deleteNode(root->left , key);
         } else if(key == root->val){
@@ -33,19 +33,16 @@ public:
                 TreeNode* temp = root->right;
                 delete root;
                 return temp;
-                
             } else if(!root->right){
                 TreeNode* temp = root->left;
                 delete root;
                 return temp;
-
             } else{
                 root->val = minVal(root->right);
                 root->right = deleteNode(root->right , root->val);
             }
+        }
 
-        }        
-        
         return root;
     }
 };
