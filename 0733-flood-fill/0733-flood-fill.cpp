12841ee7e427c7ede8c3308b06501a07b7afa060
Karan <134791021+Karan02204@users.noreply.github.com>
2026-03-19 10:40:12 +0530

    Time: 0 ms (100%), Space: 18.2 MB (43.69%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int r= image.size();
+        int r = image.size();
         int c = image[0].size();
 
-        queue<pair<int,int>> q;
+        queue<pair<int , int>> q;
         q.push({sr,sc});
+
         int pixel = image[sr][sc];
         image[sr][sc] = color;
 
-        vector<int> dx = {0,-1,0,1};
-        vector<int> dy = {1,0,-1,0};
+        vector<int> dx = {0 , -1 , 0 , 1};
+        vector<int> dy = {1 , 0 , -1 , 0};
 
         while(!q.empty()){
             int size = q.size();
-
+            
             for(int i=0;i<size;i++){
-                pair<int,int> node = q.front();
+                pair<int , int> node = q.front();
                 q.pop();
 
                 for(int j=0;j<4;j++){
                     int x = node.first + dx[j];
                     int y = node.second + dy[j];
 
-                    if(x>=0 && y>=0 && x<r && y<c && image[x][y] == pixel  && image[x][y]!=color){
-                        q.push({x,y});
+                    if(x>=0 && y>=0 && x<r && y<c && image[x][y]== pixel && image[x][y]!=color){
                         image[x][y] = color;
+                        q.push({x,y});
                     }
-                }
+                } 
             }
         }
 
