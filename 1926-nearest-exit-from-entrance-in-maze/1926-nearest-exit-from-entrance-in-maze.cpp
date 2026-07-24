36164a45de3d645bd3b8e1cbb27e6adab20f42b0
Karan <134791021+Karan02204@users.noreply.github.com>
2026-07-24 15:09:03 +0530

    Time: 4 ms (78.25%), Space: 35.7 MB (60.28%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -10,43 +10,38 @@ public:
         vector<int> dx = {0 , -1 , 0 , 1}; 
         vector<int> dy = {1 , 0 , -1 , 0};
 
-        queue<pair<int,int>> q;
-        q.push({entrance[0] , entrance[1]});
+        queue<pair<int,pair<int,int>>> q;
+        q.push({0,{entrance[0] , entrance[1]}});
         maze[entrance[0]][entrance[1]] = '+';
-
-        int answer = 0;
         
 
         while(!q.empty()){
-            int size = q.size();
 
-            for(int i=0;i<size;i++){
+            auto node = q.front();
+            q.pop();
 
-                auto node = q.front();
-                q.pop();
+            int steps = node.first;
 
-                if((node.first == 0 || node.second == 0 || node.first == r-1 || node.second == c-1) && (node.first != entrance[0] ||node.second!=entrance[1])){
+            if((node.second.first == 0 || node.second.second == 0 || node.second.first == r-1 || node.second.second == c-1) && 
+            (node.second.first != entrance[0] || node.second.second != entrance[1])){
 
-                    return answer;
+                return steps;
 
-                }
-
-                for(int j=0;j<4;j++){
-
-                    int x = node.first + dx[j];
-                    int y = node.second + dy[j];
-
-                    if(x >=0 && y >= 0 && x < r && y < c && maze[x][y] == '.'){
-
-                        q.push({x,y});
-                        maze[x][y] = '+';
-
-                    }
-
-                }
             }
 
-            answer++;
+            for(int j=0;j<4;j++){
+
+                int x = node.second.first + dx[j];
+                int y = node.second.second + dy[j];
+
+                if(x >=0 && y >= 0 && x < r && y < c && maze[x][y] != '+'){
+
+                    q.push({steps+1 , {x,y}});
+                    maze[x][y] = '+';
+
+                }
+
+            }
 
         }
 
